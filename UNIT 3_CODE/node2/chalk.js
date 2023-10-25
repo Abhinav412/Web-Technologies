8f4c6074478ea97e21a89d0c5609d05d2cc3cd47
Abhinav <abhinavdasu0412@gmail.com>
2023-10-25 10:07:31 +0530

    Update chalk.js

--- UNIT 3_CODE/node2/chalk.js
+++ UNIT 3_CODE/node2/chalk.js
@@ -2,3 +2,4 @@ import chalk from 'chalk'
 console.log(chalk.blue("Hello world"));
 console.log(chalk.red("Node Js"));
 console.log(chalk.blue.bgRed.bold("Hello Chalk World"));
+console.log(chalk.red.underline.bold("Node Js"));
