3c0ba724f0b40e070a7059808db3d73df90f29f3
Abhinav <abhinavdasu0412@gmail.com>
2023-10-25 09:53:40 +0530

    Update p3.js

--- UNIT 3_CODE/node2/p3.js
+++ UNIT 3_CODE/node2/p3.js
@@ -1,2 +1,4 @@
-import add from '.\mylib1.mjs'
-console.log("Add 3 and 5",+add(3,5));
+import add from './mylib1.mjs'
+import sub from './mylib1.mjs'
+//console.log("Add 3 and 5",+add(3,5));
+console.log("Sub 3 and 5",sub(5,3));
