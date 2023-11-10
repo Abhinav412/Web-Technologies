27e5eb7754f7f2ae9fb49a2b80762c953c16795f
Abhinav <abhinavdasu0412@gmail.com>
2023-11-10 09:31:46 +0530

    Create post.js

---
+++ UNIT 3_CODE/node2/post.js
@@ -0,0 +1,40 @@
+var http=require('http');
+var url=require('url')
+var fs=require('fs');
+var qs=require('querystring')
+var MongoClient= require('mongodb').MongoClient;
+http.createServer(function(request,response){
+    if(request.method=='POST'){
+        var myurl=url.parse(request.url);
+        var query=myurl.query;
+        var qobj=qs.parse(query);
+        let body=[];
+        request.on('data',(chunk)=>{
+            body.push(chunk);
+            console.log(chunk.toString())
+        })
+        .on('end',()=>{
+            body=Buffer.concat(body).toString();
+            console.log(body);
+        })
+    const { MongoClient } = require('mongodb');
+const url1 = "mongodb://127.0.0.1:27017/university";
+let client;
+MongoClient.connect(url1)
+    .then((connectedClient) => {
+        client = connectedClient;
+    })
+    .then((res) => {
+        const collection = client.db("university").collection("student");
+        return collection.insertOne(JSON.parse(body))
+    })
+    .then((result) => {
+        console.log("1 document inserted");
+        client.close();
+    })
+    .catch((err) => {
+        console.error("An error occurred:", err);
+    })
+}
+    }).listen(8080)
+console.log("Server is up and running");
